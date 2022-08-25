e422bc555d961cac5289d0e23675d737530c1963
DarkMortal <chodab74@gmail.com>
2022-08-26 00:08:02 +0530

    Added DSA

---
+++ DSA/PowerSUM.py
@@ -0,0 +1,14 @@
+import math
+
+def recurse(X,N,arr):
+    if X == 0:
+        return 1
+    if len(arr) == 0 or X<0:
+        return 0
+    return recurse(X-(arr[-1]**N),N,arr[0:-1])+recurse(X,N,arr[0:-1])
+
+def powerSum(X, N):
+    arr = list(range(1,int(math.pow(X,1/N))+1))
+    return recurse(X,N,arr)
+
+print(powerSum(10,2))
