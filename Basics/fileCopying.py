a3dc9b6198f19f9087da54d0537b745ae686e388
Dark Mortal <chodab74@gmail.com>
2023-05-23 22:10:45 +0530

    Fixed merge conflicts

--- Basics/fileCopying.py
+++ Basics/fileCopying.py
@@ -3,19 +3,23 @@ import os
 filePath1 = os.path.join(os.getcwd(), "../Files", "File1.xml")
 filePath2 = os.path.join(os.getcwd(), "../Files", "File2.xml")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        file1=open(filePath1,'r')
-        file2=open(filePath2,'w')
-        for line in file1.readlines():
-            #s=line.rstrip() #removes trailing whitespace at the end of line
-            #rstrip=strip from right 
-            #lstrip=strip from left
-            #strip=strip from both sides
-            file2.write(line)
-        file1.close()
-        file2.flush()
-        file2.close()
-        print("File saved successfully")
+        with open(filePath1, 'r') as file1:
+            with open(filePath2, 'w') as file2:
+                for line in file1.readlines():
+                    #s=line.rstrip() #removes trailing whitespace at the end of line
+                    #rstrip=strip from right
+                    #lstrip=strip from left
+                    #strip=strip from both sides
+                    file2.write(line)
+                file2.flush()
+                file2.close()
+            file1.close()
+            print("File saved successfully")
     except FileNotFoundError:
-        print("File not Found")
+        print("File not found")
+    except Exception:
+        print(
+            "There was some error.\nMake sure you have proper write priviledges to the File"
+        )
--- Basics/fileHandling.py
+++ Basics/fileHandling.py
@@ -1,48 +1,34 @@
-<<<<<<< HEAD:fileHandling.py
-if __name__ == "__main__":
-    try:
-        file = open("./Files/Text.txt", 'r')
-=======
 import os
-
 filePath = os.path.join(os.getcwd(), "../Files", "Text.txt")
 
-if __name__=="__main__":
+if __name__ == "__main__":
     try:
-        file=open(filePath,'r')
->>>>>>> 5362421cfed8ac437b1786aae1cfa008e427c9e0:Basics/fileHandling.py
+        file = open(filePath, 'r')
         #file.seek(n) puts file put pointer at n
+
+        # reading the file line by line
         contents = file.readline()
         while contents:
-<<<<<<< HEAD:fileHandling.py
             print(contents)
             contents = file.readline()
         file.close()
+
+        # reading the file ad once
+        print(file.read())
+
     except FileNotFoundError:
-        file = open("./Files/Text.txt", 'w')
+        file = open(filePath, 'w')
         file.write("What's the matter clown?\n" +
                    "Are you angry, humiliated, is that it?\n" +
                    "Fool, you don't know what humiliation is.")
         file.flush()
         file.close()
         print("File was generated")
-    except:
+
+        with open(filePath, 'r') as fileRead:
+            print(fileRead.read())
+    
+    except Exception:
         print(
             "There was some error.\nMake sure you have proper write priviledges to the File"
         )
-=======
-            print(contents,end='')
-            contents=file.readline()
-        print()
-        file.close()
-    except FileNotFoundError:
-        file=open(filePath,'w')
-        file.write("What's the matter clown?\n"+
-        "Are you angry, humiliated, is that it?\n"+
-        "Fool, you don't know what humiliation is.")
-        file.flush()
-        file.close()
-        print("File was generated")
-    except Exception:
-        print("There was some error.\nMake sure you have proper write priviledges to the File")
->>>>>>> 5362421cfed8ac437b1786aae1cfa008e427c9e0:Basics/fileHandling.py
